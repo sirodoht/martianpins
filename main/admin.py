224cdf7fce4225556adcef7f531b3227a5ac5d8e
Theodore Keloglou <theodorekeloglou@gmail.com>
2019-12-05 05:44:26 +0200

    add admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from main import forms, models
+
+
+class MartianUserAdmin(UserAdmin):
+    add_form = forms.MartianUserCreationForm
+    form = forms.MartianUserChangeForm
+    model = models.MartianUser
+    list_display = ["email", "username", "about"]
+
+
+admin.site.register(models.MartianUser, MartianUserAdmin)
+
+
+class PinAdmin(admin.ModelAdmin):
+    list_display = ("name", "ipfs_hash")
+
+
+admin.site.register(models.Pin, PinAdmin)
