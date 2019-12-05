d0dc50504835e00ed3598b340d621bcb8c646a35
Theodore Keloglou <theodorekeloglou@gmail.com>
2019-12-05 07:52:18 +0200

    implement ipfs requests as async tasks

--- main/tasks.py
+++ main/tasks.py
@@ -1,14 +1,22 @@
 import requests
 from background_task import background
-from django.core.mail import send_mail
 
 
 @background(schedule=1)
-def ipfs_add(file_name):
-    requests.get("http://localhost:5001/api/v0/add?arg=" + file_name)
+def ipfs_add(ipfs_file_path):
+    with open(ipfs_file_path, "rb") as f:
+        response = requests.get(
+            f"http://localhost:5001/api/v0/add?arg=" + ipfs_file_path,
+            files={"path": f.read()},
+        )
+        if response.status_code == 200:
+            print("IPFS add for " + ipfs_file_path)
+            ipfs_pin_add(response.json()["Hash"])
 
 
 @background(schedule=1)
 def ipfs_pin_add(ipfs_hash):
-    print(f"pin add for '{ipfs_hash}'")
-    requests.get("http://localhost:5001/api/v0/pin/add?arg=" + ipfs_hash)
+    response = requests.get("http://localhost:5001/api/v0/pin/add?arg=" + ipfs_hash)
+    if response.status_code == 200:
+        for p in response.json()["Pins"]:
+            print(f"Pin add for {p}")
