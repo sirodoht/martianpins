9127e67aff60b110ad596a27967904c9149474bd
Theodore Keloglou <theodorekeloglou@gmail.com>
2019-12-05 05:15:48 +0200

    add pin and user models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class MartianUser(AbstractUser):
+    about = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Pin(models.Model):
+    name = models.CharField(max_length=100)
+    ipfs_hash = models.CharField(max_length=100)
