e4b40e13397212bcaf33e301d8284fcce7e8e9f8
Theodore Keloglou <theodorekeloglou@gmail.com>
2019-12-05 05:17:46 +0200

    add index and signup views

--- main/views.py
+++ main/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.views.decorators.http import require_safe
 
-# Create your views here.
+from main import forms
+
+
+@require_safe
+def index(request):
+    return render(request, "main/index.html")
+
+
+class SignUp(generic.CreateView):
+    form_class = forms.MartianUserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
