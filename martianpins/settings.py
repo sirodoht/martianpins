f1ccdd0bce88f106904044c18e012bf9976ae1f5
Theodore Keloglou <theodorekeloglou@gmail.com>
2019-12-06 05:52:10 +0200

    update default from email

--- martianpins/settings.py
+++ martianpins/settings.py
@@ -133,4 +133,4 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 
-DEFAULT_FROM_EMAIL = "martian@sirodoht.com"
+DEFAULT_FROM_EMAIL = "Marvin from Martian Pins <marvin@martianpins.com>"
